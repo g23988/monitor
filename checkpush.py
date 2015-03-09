4092fe4473efb10a5f75ffea9c1f8cc4175616d1
Kouni <kouni@LCSE.org>
2015-03-10 00:21:30 +0800

    Init

--- checkpush.py
+++ checkpush.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #coding = utf-8
 '''
- pure push channel test for NginX push Module
+pure push channel test for NginX push Module
 NginX Push Stream Module: https://github.com/wandenberg/nginx-push-stream-module
 SUB: curl -s -v 'http://10.78.78.88/sub/978'
 PUB: curl -s -v -X POST 'http://10.78.78.88/pub?id=978' -d 'Hello_Kitty'

fe9f4365bb7ce232959407badae2bbe739ae3f3c
Kouni Huang <kouni@lcse.org>
2015-03-16 01:43:59 +0800

    Merge Sub and Sub function, bug not working

--- checkpush/checkpush.py
+++ checkpush/checkpush.py
@@ -7,19 +7,51 @@ SUB: curl -s -v 'http://10.78.78.88/sub/978'
 PUB: curl -s -v -X POST 'http://10.78.78.88/pub?id=978' -d 'Hello_Kitty'
 '''
 
-import http.client, urllib.request, urllib.parse
+import http.client, urllib.request, urllib.parse, threading, requests
 
-host = '10.78.78.88'
-channel = '978'
+host = "10.78.78.88"
+channel = "978"
 messageIndex="test"
 messageText="ok"
+"""
+class SubWait(threading.Thread):
+	def __init__(self, host, channel):
+		super(SubWait, self).__init__()
+		self.host = host
+		self.channel = channel
+		self.subresp = ""
+	def SubInfo(self):
+		'''
+		#settings = { 'interval': '0', 'count':'1' }
+		url = 'http://10.78.78.88/sub/978'
+		#r = requests.get(url, params=settings, stream=True)
+		r = requests.get(url)
+		for line in r.iter_lines():
+			if line:
+				print(line)
+		return(line)'''
+		conn = http.client.HTTPConnection(self.host)
+		conn.request('GET', '/sub/'+self.channel)
+		resp = conn.getresponse()
+		self.subresp = resp.read()
+		return(self.subresp)
+"""
+class PubData(threading.Thread):
+	def __init__(self, host, channel):
+		super(PubData, self).__init__()
+		self.host = host
+		self.channel = channel
+		self.pubresp = ""
+	def PubInfo(self):
+		conn = http.client.HTTPConnection(self.host)
+		params = urllib.parse.urlencode({"test":"OK"})
+		conn.request('POST', '/pub?id='+channel, params)
+		resp = conn.getresponse()
+		#return(resp.status, resp.reason)
+		self.pubresp = resp.read()
+		return(conn.request)
+		conn.close()
 
-conn = http.client.HTTPConnection(host)
-params = urllib.parse.urlencode({'messageText':'messageText'})
-conn.request('POST', '/pub?id='+channel, params)
-resp = conn.getresponse()
-#print(resp.status, resp.reason)
-data = resp.read()
-print(data.strip())
-conn.close()
 
+#SubWait(host, channel).start()
+PubData(host, channel).start()
